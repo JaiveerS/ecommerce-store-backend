d42b5fba2c0bd480e4628bbcb9542ace493b2698
Jaiveer Singh <43753558+JaiveerS@users.noreply.github.com>
2024-09-02 13:13:32 -0400

    added query

--- src/main/java/com/jaiveer/backend/user/UserRepository.java
+++ src/main/java/com/jaiveer/backend/user/UserRepository.java
@@ -1,10 +1,14 @@
 package com.jaiveer.backend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByEmailIgnoreCase(String email);
-    
+
     boolean existsByEmailIgnoreCase(String email);
+
+    @Query(value = "SELECT email from users where id =?1", nativeQuery = true)
+    String getEmailById(Long userId);
 }
